e5a07c07a0ad4ef43d9b23c71c41ecf861360fe2
JadonXia <>
2021-04-02 17:30:42 +0000

    fixed js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let guess = 0
 
 // Below is my while loop for 'guess my age'.
 while (guess !== numb) {
-  guess = prompt('To ensure you are not a robot, enter in this code: 3925)
+  guess = prompt('To ensure you are not a robot, enter in this code: 3925')
   guess = parseInt(guess)
   if (guess === numb) {
     alert('Verified. Please proceed to the maze game.')
   } else if (guess < numb) {
     alert('Try again.')
-  } else if (guess > age) {
-    alert('Try again')
+  } else if (guess > numb) {
+    alert('Try again.')
   } else {
     alert('Please enter the code, or close the site.')
   }
